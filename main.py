60707f81957dc38e0b05bf0f8434699f9c721079
DimaTivator <82303370+DimaTivator@users.noreply.github.com>
2021-05-10 19:22:40 +0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-a = [i for i in range(n + 1)]
-for i in range(2, n + 1):
-    if a[i] != 0:
-        print(a[i])
-        for j in range(i * i, n + 1, i):
-            a[j] = 0
-
-
